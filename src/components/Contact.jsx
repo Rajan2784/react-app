2491fae1d9fe78a11c2fdbe1a5bcd8e3edf3088c
Rajan2784 <rajansingh6616@gmail.com>
2023-08-05 21:00:11 +0530

    uploaded successfully

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Contact = () => {
+
+  const [showAfterSubmit, setShowAfterSubmit] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowAfterSubmit(true);
+
+    // After 3 seconds (adjust the time as needed)
+    setTimeout(() => {
+      setShowAfterSubmit(false);
+    }, 3000);
+  };
   return (
     <div className='contact'>
       <main>
 
         <h1>Contact Us</h1>
-
-        <form>
+        {showAfterSubmit ? (
+          <div className='afterSubmit'>
+            <h1>Thank You!!</h1>
+            <p>for contacting us.....</p>
+            <p>We will reach you soon!!</p>
+          </div>
+        ):(
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Name</label>
             <input type="text" placeholder='Abc' required />
@@ -25,7 +44,9 @@ const Contact = () => {
           <button type="submit">Submit</button>
 
         </form>
+        )}
       </main>
+
     </div>
   )
 }
